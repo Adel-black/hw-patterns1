9db58d76586e1e8dd2fd70097c0993a377787fea
Adel-black <trifonova.alina@mail.ru>
2022-12-23 12:39:03 +1000

    init

--- src/test/java/ru/netology/delivery/data/DataGenerator.java
+++ src/test/java/ru/netology/delivery/data/DataGenerator.java
@@ -1,7 +1,14 @@
 package ru.netology.delivery.data;
 
+import com.github.javafaker.Faker;
+import lombok.Value;
+import lombok.val;
+
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Random;
+
 
 public class DataGenerator {
     private DataGenerator() {
@@ -14,7 +21,37 @@ public class DataGenerator {
 
     public static String generateCity() {
         var cities = new String[]{
-
+                return city;
         }
     }
+    public static String generateName(String locale) {
+        // TODO: добавить логику для объявления переменной name и задания её значения, для генерации можно
+        // использовать Faker
+        return name;
+    }
+
+    public static String generatePhone(String locale) {
+        // TODO: добавить логику для объявления переменной phone и задания её значения, для генерации можно
+        // использовать Faker
+        return phone;
+    }
+
+    public static class Registration {
+        private Registration() {
+        }
+
+        public static UserInfo generateUser(String locale) {
+            // TODO: добавить логику для создания пользователя user с использованием методов generateCity(locale),
+            // generateName(locale), generatePhone(locale)
+            return user;
+        }
+    }
+
+    @Value
+    public static class UserInfo {
+        String city;
+        String name;
+        String phone;
+    }
+}
 }
